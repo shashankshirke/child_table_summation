72eae609b0ca962eaf3bdad367a264546785fc22
Frappe 15 <frappe15@ip-172-31-13-20.ap-south-1.compute.internal>
2024-06-09 22:43:00 +0530

    First commit

---
+++ child_table_summation/child_table_summation/doctype/child_table_summation/child_table_summation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Shashank Shirke and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Child Table Summation", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ child_table_summation/child_table_summation/doctype/child_table_summation/test_child_table_summation.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Shashank Shirke and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestChildTableSummation(FrappeTestCase):
+	pass
